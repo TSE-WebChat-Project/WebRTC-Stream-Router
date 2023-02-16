f43a49891e4b80648143e290af7edb7be0132c22
Cabe Towers <cabe.towers@gmail.com>
2023-02-16 13:15:13 +0000

    Added connection routing and renegotiation

--- index.ts
+++ index.ts
@@ -41,15 +41,16 @@ class Request{
 
 // Generate socket responses
 class Response {
-    static INVALID_REQUEST: string = JSON.stringify({"message": "Invalid request", "error": true});
-    static AUTH_FAILED: string = JSON.stringify({"message": "Invalid auth", "error": true});
+    static INVALID_REQUEST: string = JSON.stringify({command: "", error: true});
+    static AUTH_FAILED: string = JSON.stringify({message: "Invalid auth", "error": true});
+    static RENEGOTIATE_RTC: string = JSON.stringify({message: "renegotiate", renegotiate: true, error: false});
 
     static generateAnswerResponse(answer: any){
-        return JSON.stringify({"message": "SDP answer", "sdp": answer, "error": false})
+        return JSON.stringify({message: "SDP answer", "sdp": answer, error: false})
     }
 
     static generateIceMessage(candidate: RTCIceCandidate){
-        return JSON.stringify({"message": "New ICE Candidate", "candidate": candidate, "error": false})
+        return JSON.stringify({message: "New ICE Candidate", candidate: candidate, error: false})
     }
 }
 
@@ -58,6 +59,8 @@ class Client {
     private id_: string|null = null;
     private socket: WebSocket;
     private peerConnection: RTCPeerConnection;
+    private clientStream: MediaStream|null;
+    private isAdmin: boolean = false;
 
     constructor(socket) {
         this.socket = socket;
@@ -75,11 +78,15 @@ class Client {
             }
         };
 
+        this.peerConnection.ontrack = async (event) => {
+            Debug.log("Got track", 5);
+            [this.clientStream] = event.streams;
+        };
+
         this.peerConnection.onicecandidate = event => {
             // Check if the event has a candidate property
-            Debug.log("Self ICE outside");
             if (event.candidate) {
-                Debug.log("Self ICE");
+                Debug.log("Sending ICE", 5);
                 // Send the ICE candidate to the receiver over a signaling channel
                 this.sendIce(event.candidate);
             }
@@ -90,6 +97,21 @@ class Client {
         return this.id_
     }
 
+    get mediaStream(){
+        return this.clientStream;
+    }
+
+    public addMediaStream(stream: MediaStream){
+        stream.getTracks().forEach(track => {
+            Debug.log("Adding media track to user" + this.id_, 4);
+            this.peerConnection.addTrack(track, stream);
+        });
+    }
+
+    public renegotiateWebRTC(){
+        this.socket.send(Response.RENEGOTIATE_RTC);
+    }
+
     private getAuthentication(req: Request){
         if(this.id_ == null){
             this.id_ = req.id;
@@ -98,8 +120,12 @@ class Client {
             return false;
         }
 
+        if(req.id === "admin"){
+            this.isAdmin = true;
+        }
+
         if(req.token == "testauth"){ // ToDo: implement authentication
-            Debug.log("Authenticated", 4);
+            Debug.log("Authenticated user" + this.id_, 4);
             // this.send(JSON.stringify({"message": "Authenticated", "error": false}));
             return true;
         }
@@ -124,20 +150,45 @@ class Client {
             Debug.log("Auth failed", 4);
             this.socketSend(Response.AUTH_FAILED);
             this.socket.close();
+            this.peerConnection.close();
+            return;
+        }
+
+        if(this.isAdmin){ // Process admin control commands
+            this.adminCommands(req);
             return;
         }
 
         switch (req.command) {
             case "offer":
-                Debug.log("Offer");
+                Debug.log("Offer", 5);
                 this.processOffer(req.data);
                 break;
             case "iceCandidate":
-                Debug.log("ICE");
+                Debug.log("ICE", 5);
                 if(req.data.candidate){
                     this.peerConnection.addIceCandidate(req.data);
                 }
                 break;
+        }
+    }
+    
+    private adminCommands(req: Request){
+        Debug.log("Admin command", 5);
+        switch (req.command){
+            case "connect": // Connect a source users stream to other users
+                Debug.log("Connecting streams", 4);
+                let srcStream = getClientById(req.data[0]).mediaStream;
+                for (let i = 1; i < req.data.length; i++) {
+                    let user = getClientById(req.data[i]);
+                    if(!user){Debug.log("No such user " + req.data[i]);}
+                    user.addMediaStream(srcStream);
+                    user.renegotiateWebRTC();
+                }
+                break;
+            case "disconnect":
+                break;
+                // Todo: Add redistribution logic
         }
     }
 
@@ -164,13 +215,29 @@ function removeItem<T>(arr: Array<T>, value: T): Array<T> {
     return arr;
 }
 
-let clients: Client[] = []
+let clients: Client[] = [];
+
+function getClientById(id: string): Client|null{
+    let result: Client|null = null;
+    clients.forEach((client) => {
+        if(client.id === id){
+            result = client;
+        }
+    });
+
+    return result;
+}
 
 server.on('connection', (socket: WebSocket) => {
-    Debug.log("Got connection");
+    Debug.log("Got connection", 3);
 
     let client = new Client(socket);
 
     clients.push(client);
+    
+    socket.onclose = () => { // Remove client from list on disconnect
+        removeItem(clients, client);
+    };
+    
 
 });
